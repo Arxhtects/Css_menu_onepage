a485b7e1c1b920c233f01738100c2a8d34a5db01
Archtects <scott.ch4mb3rs@gmail.com>
2017-02-14 06:41:11 +0000

    Add files via upload

---
+++ function.js
@@ -0,0 +1,8 @@
+   function transition() {
+       var divtran = document.getElementById('mobile_menu');
+       
+       if (divtran.style.height == '100vh') 
+           divtran.style.height = '0vh'
+       else 
+           divtran.style.height = '100vh'
+}
