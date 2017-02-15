b8487080f3c58178a565c09a230164ae6d286901
Archtects <scott.ch4mb3rs@gmail.com>
2017-02-15 13:32:47 +0000

    Js & html fix
    now closes the menu properly when you click on a link. Thanks to helpful user on stackoverflow : "Richie Fredicson"

--- function.js
+++ function.js
@@ -1,8 +1,22 @@
-   function transition() {
+function transition() {
        var divtran = document.getElementById('mobile_menu');
-       
-       if (divtran.style.height == '100vh') 
-           divtran.style.height = '0vh'
-       else 
+       var menuDrop = document.querySelectorAll(".mobilemenu");
+       var menuOpen = false;
+       if (divtran.style.height == '100vh') {
+           divtran.style.height = '0vh';
+           menuOpen = false;
+       }
+       else {
            divtran.style.height = '100vh'
+           menuOpen = true;
+       }
+       for(var i=0;i<menuDrop.length;i++){
+          if(menuOpen) {
+            menuDrop[i].classList.add("act");
+          }
+          else {
+            menuDrop[i].classList.remove("act");
+          }
+
+       }
 }
