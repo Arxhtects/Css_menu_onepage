c0e0a6a507b9cba4b6922a8417f2ad699db55e4f
Archtects <scott.ch4mb3rs@gmail.com>
2018-10-02 11:18:00 +0100

    spacing fix

--- function.js
+++ function.js
@@ -1,22 +1,19 @@
 function transition() {
-       var divtran = document.getElementById('mobile_menu');
-       var menuDrop = document.querySelectorAll(".mobilemenu");
-       var menuOpen = false;
-       if (divtran.style.height == '100vh') {
-           divtran.style.height = '0vh';
-           menuOpen = false;
-       }
-       else {
-           divtran.style.height = '100vh'
-           menuOpen = true;
-       }
-       for(var i=0;i<menuDrop.length;i++){
-          if(menuOpen) {
-            menuDrop[i].classList.add("act");
-          }
-          else {
-            menuDrop[i].classList.remove("act");
-          }
-
-       }
+  var divtran = document.getElementById('mobile_menu');
+  var menuDrop = document.querySelectorAll(".mobilemenu");
+  var menuOpen = false;
+    if (divtran.style.height == '100vh') {
+      divtran.style.height = '0vh';
+      menuOpen = false;
+    } else {
+      divtran.style.height = '100vh'
+      menuOpen = true;
+    }
+    for(var i=0;i<menuDrop.length;i++){
+      if(menuOpen) {
+        menuDrop[i].classList.add("act");
+      } else {
+        menuDrop[i].classList.remove("act");
+      }
+    }
 }
